c1f1707022f8ff683e03a3d9b49dc0931f0073cb
sarangkamble <sarangkamble.1994@gmail.com>
2020-04-30 21:37:28 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-coronavirus-trackers
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-coronavirus-trackers)
---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HomeComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }

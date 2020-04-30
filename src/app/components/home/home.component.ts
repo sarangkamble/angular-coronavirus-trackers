fa59ba059bdee081ae8eaafe8b053c3d17ccb898
sarangkamble <sarangkamble.1994@gmail.com>
2020-05-01 01:28:03 +0530

    Successfully implemented pie chart and column chart

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,63 +1,73 @@
-import { Component, OnInit } from '@angular/core';
-import { DataService } from '../../services/data.service';
-import { GolbalDataSummary } from '../models/global.data';
-import { GoogleChartInterface } from 'ng2-google-charts';
+import { Component, OnInit } from "@angular/core";
+import { DataService } from "../../services/data.service";
+import { GolbalDataSummary } from "../models/global.data";
+import { GoogleChartInterface } from "ng2-google-charts";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
-
-
   totalConfirmed = 0;
   totalDeaths = 0;
   totalRecovered = 0;
   totalActive = 0;
   golbalData: GolbalDataSummary[];
   pieChart: GoogleChartInterface = {
+    chartType: "PieChart"
+  };
 
-    chartType: 'PieChart'
+  columnChart: GoogleChartInterface = {
+    chartType: "ColumnChart"
+  };
 
+  constructor(private dataService: DataService) {}
+
+  initChart() {
+    let datatable = [];
+    datatable.push(["Country", "Cases"]);
+    this.golbalData.forEach(cs => {
+      if (cs.confirmed > 2000) {
+        datatable.push([cs.country, cs.confirmed]);
+      }
+    });
+// 
+    this.pieChart = {
+      chartType: "PieChart",
+      dataTable: datatable,
+      //firstRowIsData: true,
+
+      options: {
+        height: 500
+      }
+    };
+    this.columnChart = {
+      chartType: "ColumnChart",
+      dataTable: datatable,
+      //firstRowIsData: true,
+
+      options: {
+        height: 500
+      }
+    };
   }
-
-  constructor(private dataService: DataService) { }
-
-  initChart(){
-  let datatable = [];
-  datatable.push(["Country", "Cases"])
-  this.golbalData.forEach(cs=>{
-    datatable.push[
-      cs.country, cs.confirmed
-    ]
-  })
-
-  this.pieChart = {
-  chartType: 'PieChart',
-  dataTable: datatable,
-  //firstRowIsData: true,
-  options: {'Country': 'Cases'},
-};
-}
 
   ngOnInit() {
-
     this.dataService.getGlobalData().subscribe({
-      next: (result) => {
-//        console.log(result);
+      next: result => {
+        //        console.log(result);
         this.golbalData = result;
-        result.forEach(cs=>{
-          if(!Number.isNaN(cs.confirmed)){
-            this.totalActive += cs.active
-            this.totalConfirmed += cs.confirmed
-            this.totalDeaths += cs.deaths
-            this.totalRecovered += cs.recovered
+        result.forEach(cs => {
+          if (!Number.isNaN(cs.confirmed)) {
+            this.totalActive += cs.active;
+            this.totalConfirmed += cs.confirmed;
+            this.totalDeaths += cs.deaths;
+            this.totalRecovered += cs.recovered;
           }
-        })
+        });
         this.initChart();
       }
-    })
+    });
   }
-
 }

703052a12a94205ceb0e5c83fd5e8f57150a6b0f
sarangkamble <sarangkamble.1994@gmail.com>
2020-05-19 16:28:40 +0530

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,13 @@ import { CountriesComponent } from './components/countries/countries.component';
 import { DataService } from './services/data.service';
 import { HttpClientModule } from '@angular/common/http';
 import { DashboardCardComponent } from './components/dashboard-card/dashboard-card.component';
+import { IndiaService } from './services/india.service';
+import { IndiaComponent } from './components/india/india.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, NgbModule, AppRoutingModule, HttpClientModule, GoogleChartsModule],
-  declarations: [ AppComponent, HomeComponent, NavbarComponent, CountriesComponent, DashboardCardComponent ],
+  declarations: [ AppComponent, HomeComponent, NavbarComponent, CountriesComponent, DashboardCardComponent, IndiaComponent ],
   bootstrap:    [ AppComponent ],
-  providers: [DataService]
+  providers: [DataService, IndiaService]
 })
 export class AppModule { }
